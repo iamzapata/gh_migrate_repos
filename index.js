61dafed32de89437e5103b030fd27eedf426bf7b
Andres Zapata <me@azapata.io>
2022-10-07 17:48:00 +0200

    Complete repo cloning

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 #!/usr/bin/env zx
-
 import "zx/globals"
 
-await $`git st`
-
 try {
-  $.verbose = false
+  cd("repos/")
 
-  let repos = await $`gh repo list --json name --limit 1000`
+  $.verbose = false
+  let repos = await $`gh repo list --json name --limit 1000 --source`
+
+  $.verbose = true
+  repos = JSON.parse(repos)
 
   for (let repo of repos) {
-    console.log(repo.name)
+    chalk.blue(`Cloning ${repo.name}`)
+    try {
+      await $`gh repo clone ${repo.name}`
+      chalk.blue(`${repo.name} Cloned`)
+    } catch (e) {
+      chalk.red(`Error cloning ${repo.name}`, e)
+      continue
+    }
   }
 } catch (p) {
   console.log(`Exit code: ${p.exitCode}`)
